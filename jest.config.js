088f3c136147009a9b41b922ee9a160c0d51a262
isadorabrito <isadoradpbrito@gmail.com>
2023-08-10 13:47:10 -0300

    refactor: config and test script

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,9 @@
-export const roots = ["<rootDir>/src", "<rootDir>/tests"];
-export const collectCoverageFrom = [
-  "<rootDir>/src/**/*.{ts,tsx}",
-  "!<rootDir>/src/main/**/*",
-  "!<rootDir>/src/**/index.ts",
-  "!**/*.d.ts",
-];
-export const coverageDirectory = "coverage";
-export const setupFilesAfterEnv = ["<rootDir>/src/main/config/jest-setup.ts"];
-export const testPathIgnorePatterns = [
-  "<rootDir>/node_modules/",
-  "<rootDir>/tests/e2e/cypress",
-];
-export const testEnvironment = "jsdom";
-export const transform = {
-  ".+\\.(ts|tsx)$": "ts-jest",
-};
-export const moduleNameMapper = {
-  "@/tests/(.*)": "<rootDir>/tests/$1",
-  "@/(.*)": "<rootDir>/src/$1",
-  "\\.scss$": "identity-obj-proxy",
+module.exports = {
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}"],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  transform: {
+    ".+\\.(ts|tsx)$": "ts-jest",
+  },
 };
